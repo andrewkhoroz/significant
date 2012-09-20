81001b8389e71b012a47d507cf40cb668caca340
andrewkhoroz <andriyhoroz@gmail.com>
2012-09-20 23:48:09 +0300

    Add html5 controller

--- application/controllers/HTML5Controller.php
+++ application/controllers/HTML5Controller.php
@@ -7,13 +7,23 @@ class HTML5Controller extends Zend_Controller_Action {
      *
      */
     public function indexAction() {
-        $this->view->title = '15 CSS3 Navigation and Menu Tutorials and Techniques';
-        $this->view->headTitle('15 CSS3 Navigation and Menu Tutorials and Techniques');
+        $this->view->title = 'HTML5';
+        $this->view->headTitle('HTML5');
     }
 
     public function offlineAction() {
         $this->view->title = 'Offline Applications';
         $this->view->headTitle('Offline Applications');
+    }
+
+    public function articleAction() {
+        $this->view->title = '15 CSS3 Navigation and Menu Tutorials and Techniques';
+        $this->view->headTitle('15 CSS3 Navigation and Menu Tutorials and Techniques');
+    }
+
+    public function layoutAction() {
+        $this->view->title = 'Layout (Mergevych)';
+        $this->view->headTitle('Layout (Mergevych)');
     }
 
 }
